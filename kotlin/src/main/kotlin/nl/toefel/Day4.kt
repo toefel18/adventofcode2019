8edc497322bb9ba412dffe7ef4092f5870c6a879
toefel <toefel18@gmail.com>
2019-12-04 22:12:49 +0100

    Cleaned up

--- kotlin/src/main/kotlin/nl/toefel/Day4.kt
+++ kotlin/src/main/kotlin/nl/toefel/Day4.kt
@@ -1,37 +1,23 @@
 package nl.toefel
 
 fun main() {
-    val (start, max) = listOf(152085, 670283)
-    val options = (start..max).map { it.toString() }.filter { hasAdjacentDigits(it) }.filter { onlyIncreasing(it) }
+    val range = (152085..670283).map { it.toString() }.filter { onlyIncreasing(it) }
+
+    val options = range.filter { hasAdjacentDigits(it) }
     println("part 1 = ${options.size}")
 
-    val part2Options = (start..max)
-        .map { it.toString() }
-        .filter { hasAdjacentDigitsNotTakingSequencesLargerThan2IntoAccount(it) }
-        .filter { onlyIncreasing(it) }
-
+    val part2Options = range.filter { hasSinglePairOfAdjacentDigits(it) }
     println("part 2 = ${part2Options.size}")
 }
 
-fun hasAdjacentDigits(pwd: String): Boolean {
-    for (i in 1 until pwd.length) {
-        if (pwd[i-1] == pwd[i]) return true
-    }
-    return false
-}
+fun hasAdjacentDigits(pwd: String) = (1 until pwd.length).any { pwd[it - 1] == pwd[it] }
+fun onlyIncreasing(pwd: String) = (1 until pwd.length).all {pwd[it - 1].toInt() <= pwd[it].toInt()}
 
-fun onlyIncreasing(pwd: String): Boolean {
-    for (i in 1 until pwd.length) {
-        if (pwd[i-1].toByte() > pwd[i].toByte()) return false
-    }
-    return true
-}
-
-fun hasAdjacentDigitsNotTakingSequencesLargerThan2IntoAccount(pwd: String): Boolean {
+fun hasSinglePairOfAdjacentDigits(pwd: String): Boolean {
     for (i in 1 until (pwd.length - 1)) {
-        if (pwd[i-1] == pwd[i] && pwd[i] == pwd[i+1]) {
+        if (pwd[i - 1] == pwd[i] && pwd[i] == pwd[i + 1]) {
             // filter sequences longer than 2 and try again
-            return hasAdjacentDigitsNotTakingSequencesLargerThan2IntoAccount(pwd.filter { it != pwd[i] })
+            return hasSinglePairOfAdjacentDigits(pwd.filter { it != pwd[i] })
         }
     }
     // no sequences longer than 3 found, now check if there is a group
