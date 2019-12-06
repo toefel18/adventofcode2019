51bcdf56158053dd10b89d5317ec3fd20af5020e
toefel <toefel18@gmail.com>
2019-12-06 16:51:01 +0100

    Day6 part 2

--- kotlin/src/main/kotlin/nl/toefel/Day6.kt
+++ kotlin/src/main/kotlin/nl/toefel/Day6.kt
@@ -11,9 +11,20 @@ fun main() {
     val vertices = (edges.keys + edges.values).toSet().minus("COM")
     val orbits = vertices.map { countOrbitsTo(it, "COM", edges) }.sum()
     println("part1 = $orbits")
+
+    val sanOrbiting = orbitPath("SAN", "COM", edges).reversed()
+    val youOrbiting = orbitPath("YOU", "COM", edges).reversed()
+    val pathsJoinAtObject = youOrbiting.find { sanOrbiting.indexOf(it) > 0 }
+    val stepsBetween = sanOrbiting.indexOf(pathsJoinAtObject) + youOrbiting.indexOf(pathsJoinAtObject)
+    println("part2 = $stepsBetween")
 }
 
 fun countOrbitsTo(from: String, to: String, edges: Map<String, String>): Long {
     val next = edges[from]
     return if (next == to) return 1 else 1 + countOrbitsTo(next!!, to, edges)
+}
+
+fun orbitPath(from: String, to: String, edges: Map<String, String>): List<String> {
+    val next = edges[from]
+    return if (next == to) return listOf(next) else orbitPath(next!!, to, edges) + next
 }
