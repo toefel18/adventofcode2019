2410204372da588843727acc5efc517e96d85f19
toefel <toefel18@gmail.com>
2019-12-13 00:24:29 +0100

    Day 12 try for part 2 with hints of forum

--- kotlin/src/main/kotlin/nl/toefel/day12/Day12.kt
+++ kotlin/src/main/kotlin/nl/toefel/day12/Day12.kt
@@ -12,7 +12,7 @@ data class Point3D(val x: Int, val y: Int, val z: Int) {
 
     operator fun plus(other: Point3D) = Point3D(this.x + other.x, this.y + other.y, this.z + other.z)
     fun energy(): Int = x.absoluteValue + y.absoluteValue + z.absoluteValue
-    fun digitString() : String = x.toString().padStart(3) + y.toString().padStart(3) + z.toString().padStart(3)
+    fun digitString(): String = x.toString().padStart(3) + y.toString().padStart(3) + z.toString().padStart(3)
 }
 
 data class Moon(val pos: Point3D, val velocity: Point3D) {
@@ -29,9 +29,10 @@ fun List<Moon>.print(step: Int) {
     forEach { println(it) }
 }
 
+
 fun main() {
-    part1()
-//    part2()
+//    part1()
+    part2()
 }
 
 private fun part1() {
@@ -57,28 +58,73 @@ private fun part1() {
     println("Part 1 total energy in system: " + currentMoons.map { it.totalEnergy() }.sum())
 }
 
-val ZeroVelocity = Point3D(0,0,0)
-
-
-fun List<Moon>.printVelocity(step: Int) {
-    println("Step ${step.toString().padStart(10)} ${this.map { it.velocity.digitString() }.joinToString()}")
-    forEach { println(it) }
-}
-
 private fun part2() {
-    val moons = File(ClassLoader.getSystemResource("day12-test-input.txt").file).readLines()
+    val moons = File(ClassLoader.getSystemResource("day12-input.txt").file).readLines()
         .map { it.split(", |<|>|=".toRegex()) }
         .map { Moon(Point3D(it[2].toInt(), it[4].toInt(), it[6].toInt()), Point3D(0, 0, 0)) }
 
     var currentMoons = moons
 
+    // maps moon.x,y,z to the list of velocities
+    // for example:
+    // 0.x 1 2 1 2 1 2
+    // 0.y 3 4 3 4
+    // ..
+    // 3.z 9 8 9 8
+    val velocityValues = mutableMapOf<String, MutableList<Long>>()
 
-    generateSequence(1) { it + 1 }.forEach { step ->
+    // initialize first values (all 0)
+    currentMoons.writeVelocityTo(velocityValues)
+
+    // find the repeating sequence per moon per velocity
+    var periods = velocityValues.findRepeatingSequenceLengths()
+
+    // keep computing values until we find a period (repeated sequence) for each moons velocity axis
+    generateSequence(1) { it + 1 }.takeWhile { !periods.values.all { it > 0 } }.forEach { step ->
         currentMoons = currentMoons.map { it.applyGravity(currentMoons.minus(it)) }
+        currentMoons.writeVelocityTo(velocityValues)
+        periods = velocityValues.findRepeatingSequenceLengths()
+    }
+
+    periods.forEach { (k, v) -> println("$k $v") }
+
+    // now compute the least common multiple of all the sequence lengths
+    val leastCommonMultipleOfAllSequences = periods.values.reduce { acc, next -> lcm(acc, next)}
+    println("Least common multiple of all sequences = $leastCommonMultipleOfAllSequences")
+}
+
+fun List<Moon>.writeVelocityTo(velocities: MutableMap<String, MutableList<Long>>) {
+    forEachIndexed { moonNumber, moon ->
+        velocities.computeIfAbsent("$moonNumber.x") { mutableListOf() }.add(moon.velocity.x.toLong())
+        velocities.computeIfAbsent("$moonNumber.y") { mutableListOf() }.add(moon.velocity.y.toLong())
+        velocities.computeIfAbsent("$moonNumber.z") { mutableListOf() }.add(moon.velocity.z.toLong())
+    }
+}
+
+/** finds the repeating sequence per list. For example: 0, 1, 2, 0, 1, 2  will find 0, 1, 2 */
+fun MutableMap<String, MutableList<Long>>.findRepeatingSequences(): Map<String, List<Long>> {
+    return this.mapValues { findRepeatingSequence(it.value) }
+}
+
+/** finds the length of a repeating sequence, for example if [findRepeatingSequences] returns 0,1,2 then this will return 3*/
+fun MutableMap<String, MutableList<Long>>.findRepeatingSequenceLengths(): Map<String, Long> {
+    return findRepeatingSequences().mapValues { it.value.size.toLong() }
+}
+
+// greatest common divisor
+fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
+
+// least common multiple
+fun lcm(a: Long, b: Long): Long = (a * b) / gcd(a, b)
 
 
-        if (currentMoons.map { it.velocity }.all { it == ZeroVelocity }) {
-            println("Zero velocity at $step")
+fun findRepeatingSequence(intList: List<Long>): List<Long> {
+    (2 until intList.size / 2).forEach { idx ->
+        val currentPeriod = intList.subList(0, idx)
+        val nextPeriod = intList.subList(idx, 2 * idx)
+        if (currentPeriod == nextPeriod) {
+            return currentPeriod
         }
     }
+    return listOf()
 }
